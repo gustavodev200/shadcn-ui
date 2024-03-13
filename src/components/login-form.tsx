131caf1c329491ae0941c5f16350ee1dd5693a99
gustavodev200 <gustavolage200@gmail.com>
2024-03-13 15:45:57 -0300

    form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -8,6 +8,19 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
   email: z.string().email("E-mail não válido!"),
@@ -15,13 +28,64 @@ const formSchema = z.object({
 });
 
 const LoginForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <Card>
       <CardHeader>
         <CardTitle>Faça o Login</CardTitle>
         <CardDescription>Digite seus dados de login.</CardDescription>
       </CardHeader>
-      <CardContent>...</CardContent>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu e-mail" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Digite sua senha"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Entrar</Button>
+          </form>
+        </Form>
+      </CardContent>
     </Card>
   );
 };
